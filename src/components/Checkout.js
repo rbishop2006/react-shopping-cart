4e4e4741f6561c7cf0e88a09b1e9a81edefd406b
Robert Bishop <rbishop2006@gmail.com>
2020-03-01 20:47:19 -0800

    final

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Link } from "react-router-dom"
 import { useCart } from "../hooks"
 // import "../styles/products.css"
-// import { TiShoppingCart } from "react-icons/ti"
+import { TiShoppingCart } from "react-icons/ti"
 
 export default props => {
   const { cart } = useCart()
@@ -12,26 +12,29 @@ export default props => {
   }, 0)
 
   return (
-    <div>
+    <div className="checkoutWrapper">
       <div className="">
+        <div className="checkOutHeader">
+          <h1>Thank you for your order!</h1>
+        </div>
         {cart.map(item => (
-          <div className="" key={"item" + item.id}>
+          <div className="checkOutItems" key={"item" + item.id}>
             <div className="">
-              <p className="">{item.title}</p>
-              <p className="">{item.style}</p>
-              <p className="">Quantity:</p>
+              <p className="checkOutItemTitle">{item.title}</p>
+              <p className="checkOutItemStyle">{item.style}</p>
+              <p className="checkOutItemQuantity">Quantity:</p>
             </div>
-            <div className="">
+            <div className="checkOutPrice">
               <p className="">${item.price.toFixed(2)}</p>
             </div>
           </div>
         ))}
       </div>
       <div className="">
-        <div className="">
+        <div className="checkOutFooter">
           <p>SUBTOTAL</p>
           <div>
-            <p className="">${total.toFixed(2)}</p>
+            <p className="checkOutFooterPrice">${total.toFixed(2)}</p>
           </div>
         </div>
       </div>
