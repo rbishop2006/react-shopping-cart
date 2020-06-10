2899884c769c036618b96031553135d8f9b88aa9
Robert Bishop <rbishop2006@gmail.com>
2020-06-10 13:57:59 -0700

    Updates

---
+++ precache-manifest.c012eff392bd75ceeccfa5e47a1b7f38.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2b41b39e716fcc0fdc7a737c9119a3c",
+    "url": "/react-shopping-cart/index.html"
+  },
+  {
+    "revision": "fecd9b46fc1ae69f93c5",
+    "url": "/react-shopping-cart/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "8ab4063ab141c3d7509a",
+    "url": "/react-shopping-cart/static/css/main.4356959e.chunk.css"
+  },
+  {
+    "revision": "fecd9b46fc1ae69f93c5",
+    "url": "/react-shopping-cart/static/js/2.0e2ed3c2.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/react-shopping-cart/static/js/2.0e2ed3c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ab4063ab141c3d7509a",
+    "url": "/react-shopping-cart/static/js/main.4d473fc8.chunk.js"
+  },
+  {
+    "revision": "e8ef8386c7dd66dbfa25",
+    "url": "/react-shopping-cart/static/js/runtime-main.61dc67b3.js"
+  }
+]);
